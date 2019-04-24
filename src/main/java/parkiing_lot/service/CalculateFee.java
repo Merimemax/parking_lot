4e4c25413fe9519d0c113f20a0e9be204f302dae
Merih <merimemax@gmail.com>
2019-04-24 18:17:46 -0400

    removing magic numbers

--- src/main/java/parkiing_lot/service/CalculateFee.java
+++ src/main/java/parkiing_lot/service/CalculateFee.java
@@ -2,7 +2,8 @@ package parkiing_lot.service;
 
 public class CalculateFee {
 
-    public static final double PARKIING_FEE = 5.00;
+    private static final double PARKING_FEE = 5.00;
+    private static final double MAX_FEE = 30.00;
 
     public double calculateFee(String license_plate, long checkingTime){
 
@@ -15,17 +16,17 @@ public class CalculateFee {
 
         int hours   = (int) ((totalTime / (1000*60*60)) % 24);
 
-        double fee = PARKIING_FEE * hours;
+        double fee = PARKING_FEE * hours;
 
-        if(fee > 30){
-            return 30;
-        }else {
-            return fee;
-        }
+        return fee > MAX_FEE ? MAX_FEE : fee;
 
     }
 
     private boolean haveMonthlyPass(String license_plate) {
+
+        //we check our system if this license plate is registered
+        //for monthly parking subscription
+
         return false;
     }
 }
